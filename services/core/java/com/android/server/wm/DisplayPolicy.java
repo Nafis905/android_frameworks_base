836919d27ef73b97c7b393d990ede2519ad56aca
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-05-01 06:19:03 +0800

    services: Optimizing redundant keyguard checks
    Change-Id: Ibb8406b95bdd87a1e2c3adcc3b8292bd3ef99d85
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -389,6 +389,8 @@ public class DisplayPolicy {
 
     private boolean mShowingDream;
     private boolean mLastShowingDream;
+    private boolean mKeyguardShowingForPostLayout;
+    private boolean mKeyguardShowingAndNotOccludedForPostLayout;
     private boolean mDreamingLockscreen;
     private boolean mAllowLockscreenWhenOn;
 
@@ -1546,6 +1548,9 @@ public class DisplayPolicy {
 
         mAllowLockscreenWhenOn = false;
         mShowingDream = false;
+        mKeyguardShowingForPostLayout = isKeyguardShowing();
+        mKeyguardShowingAndNotOccludedForPostLayout =
+                mService.mPolicy.isKeyguardShowingAndNotOccluded();
         mIsFreeformWindowOverlappingWithNavBar = false;
         mShowingTransientInsetsTypes = 0;
         mImeInsetsConsumed = false;
@@ -1801,7 +1806,7 @@ public class DisplayPolicy {
         // started while the lockscreen was showing and remember this state
         // while the dream is showing.
         if (!mShowingDream) {
-            mDreamingLockscreen = mService.mPolicy.isKeyguardShowingAndNotOccluded();
+            mDreamingLockscreen = mKeyguardShowingAndNotOccludedForPostLayout;
         }
 
         updateSystemBarAttributes();
@@ -1847,7 +1852,7 @@ public class DisplayPolicy {
 
     private boolean shouldBeHiddenByKeyguard(WindowState win,
             @Nullable WindowState imeLayeringTarget) {
-        if (!mDisplayContent.isDefaultDisplay || !isKeyguardShowing()) {
+        if (!mDisplayContent.isDefaultDisplay || !mKeyguardShowingForPostLayout) {
             return false;
         }
 
